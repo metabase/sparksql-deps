264c8a198db3f19b283cc67b63c9f0db1d129b4e
Cam Saul <cammsaul@gmail.com>
2018-05-10 14:42:50 -0700

    Add Metabase FixedHive classes

--- project.clj
+++ project.clj
@@ -4,9 +4,12 @@
   :license {:name "Apache License 2.0"
             :url "https://raw.githubusercontent.com/metabase/sparksql-deps/master/LICENSE"}
   :dependencies [[org.apache.hadoop/hadoop-common "3.1.0"]
+                 [org.clojure/clojure "1.8.0"]
                  [org.spark-project.hive/hive-jdbc "1.2.1.spark2"
                   :exclusions [org.apache.curator/curator-framework
                                org.apache.curator/curator-recipes
                                org.apache.thrift/libfb303
                                org.apache.zookeeper/zookeeper
-                               org.eclipse.jetty.aggregate/jetty-all]]])
+                               org.eclipse.jetty.aggregate/jetty-all]]]
+  :aot :all
+  :uberjar-exclusions [#".*clojure.*"])
